c828c6de569e107f738366f3f07d66336a33e090
Meghamala <meghamala1996@gmail.com>
2024-02-13 15:27:24 -0800

    Cards OpenAPI doc creation

--- cards/src/main/java/com/springbank/cards/controller/CardsController.java
+++ cards/src/main/java/com/springbank/cards/controller/CardsController.java
@@ -2,8 +2,15 @@ package com.springbank.cards.controller;
 
 import com.springbank.cards.constants.CardsConstants;
 import com.springbank.cards.dto.CardsDto;
+import com.springbank.cards.dto.ErrorResponseDto;
 import com.springbank.cards.dto.ResponseDto;
 import com.springbank.cards.service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -15,6 +22,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 // REST APIs related to Cards Microservice
+
+@Tag(
+        name = "CRUD REST APIs for Cards in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+)
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -28,6 +40,25 @@ public class CardsController {
     @Autowired
     private ICardsService iCardsService;
 
+
+    @Operation(
+            summary = "Create Card REST API",
+            description = "REST API to create new Card inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createCards(@Valid @RequestParam
                                                        @Pattern(regexp = "(^$|[0-9]{10})", message="Mobile number must be 10 digits")String mobileNumber) {
@@ -39,6 +70,24 @@ public class CardsController {
                 .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Card Details REST API",
+            description = "REST API to fetch card details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+
     @GetMapping("/fetch")
     public ResponseEntity<CardsDto> fetchCards(@RequestParam
                                                    @Pattern(regexp = "(^$|[0-9]{10})", message="Mobile number must be 10 digits") String mobileNumber) {
@@ -49,6 +98,27 @@ public class CardsController {
                 .body(cardsDto);
     }
 
+    @Operation(
+            summary = "Update Card Details REST API",
+            description = "REST API to update card details based on a card number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateCardDetails(@Valid @RequestBody CardsDto cardsDto) {
         Boolean isUpdated = iCardsService.updateCard(cardsDto);
@@ -64,7 +134,27 @@ public class CardsController {
         }
     }
 
-
+    @Operation(
+            summary = "Delete Card Details REST API",
+            description = "REST API to delete Card details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteCardDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message="Mobile number must be 10 digits")
                                                              String mobileNumber) {
--- cards/src/main/java/com/springbank/cards/dto/CardsDto.java
+++ cards/src/main/java/com/springbank/cards/dto/CardsDto.java
@@ -1,5 +1,6 @@
 package com.springbank.cards.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -8,24 +9,45 @@ import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
+@Schema(name = "Cards",
+        description = "Schema to hold Card information"
+)
 public class CardsDto {
 
     @Pattern(regexp = "(^$|[0-9]{10})", message="Mobile number must be 10 digits")
+    @Schema(
+            description = "Mobile Number of Customer", example = "4354437687"
+    )
     private String mobileNumber;
 
     @NotEmpty(message = "Card number cannot be null or empty")
+    @Schema(
+            description = "Card Number of the customer", example = "100646930341"
+    )
     private String cardNumber;
 
     @NotEmpty(message = "Card type cannot be null or empty")
+    @Schema(
+            description = "Type of the card", example = "Credit Card"
+    )
     private String cardType;
 
     @Positive(message = "Total card limit should be greater than zero")
+    @Schema(
+            description = "Total amount limit available against a card", example = "100000"
+    )
     private int totalLimit;
 
     @PositiveOrZero(message = "Total amount used should be equal or greater than zero")
+    @Schema(
+            description = "Total amount used by a Customer", example = "1000"
+    )
     private int amountUsed;
 
     @PositiveOrZero(message = "Total available amount should be equal or greater than zero")
+    @Schema(
+            description = "Total available amount against a card", example = "90000"
+    )
     private int availableAmount;
 
 
