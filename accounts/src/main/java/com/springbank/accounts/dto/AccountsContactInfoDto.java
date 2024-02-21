0632272fcfa55985ab8bde6a1e89e4a94a5c25f1
Meghamala <meghamala1996@gmail.com>
2024-02-21 15:47:38 -0800

    refresh configs at runtime using refresh actuator

--- accounts/src/main/java/com/springbank/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/springbank/accounts/dto/AccountsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.springbank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -8,6 +10,13 @@ import java.util.Map;
 // this class makes sures there are only getter methods and no setter methods to make changes to fields
 // all params are final and java creates a constructor to initialize. Can only initialize once
 
+// converting record to class for enabling run time configuration changes
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
